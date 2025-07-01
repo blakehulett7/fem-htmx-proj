b49b83d60f51395eecb973230324aa3be79d8993
blakehulett7 <blake.hulett7@gmail.com>
2025-06-30 19:55:13 -0500

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func NewData() Data {
+	return Data{
+		Contacts: []Contact{
+			Contact{Name: "John", Email: "jd@gmail.com"},
+			Contact{Name: "Clara", Email: "cd@gmail.com"},
+		},
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	data := NewData()
+	e.Renderer = NewTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, Contact{Name: name, Email: email})
+
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
